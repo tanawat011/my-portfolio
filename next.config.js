e211ebeecdb70a183374ef18d1c4438ae3941d5e
Tanawat Pinthongpan <tanawat@magicboxasia.com>
2022-04-16 23:56:44 +0700

    fix: remove line comment

--- next.config.js
+++ next.config.js
@@ -6,15 +6,10 @@ const nextConfig = {
       process.env.NEXT_PUBLIC_API_URL,
   },
   reactStrictMode: true,
-  // images: {
-  //   dangerouslyAllowSVG: true,
-  //   contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-  // },
   images: {
     loader: 'imgix',
     path: 'https://tanawat.web.app/',
     // path: 'http://localhost:3000/',
-    // domains: ['cdn-images-1.medium.com', 'medium.com'],
   },
 }
 
